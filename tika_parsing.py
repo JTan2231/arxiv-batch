a308b96f615f2d80d021371aa65715a4ca13d9ee
julian31186 <julian31186@gmail.com>
2024-02-11 12:41:13 -0500

    Skeleton Batch

--- tika_parsing.py
+++ tika_parsing.py
@@ -1,6 +1,9 @@
 import re
 import unicodedata
+import os,shutil
+import requests
 from tika import parser
+from bs4 import BeautifulSoup
 
 def strip_accents(s):
    return ''.join(c for c in unicodedata.normalize('NFD', s)
@@ -12,39 +15,76 @@ def clean_string(s):
 
     return s
 
-parsed = parser.from_file('./iclr/pdf.pdf')
-content = parsed['content']
+def get_citations(pdf_path):
+    parsed = parser.from_file(pdf_path)
+    content = parsed['content']
 
-options = ['References', 'REFERENCES']
+    options = ['References', 'REFERENCES']
 
-reference_split = ''
-for o in options:
-    if o in content:
-        print(f'found {o}')
-        reference_split = content.split(o)[-1]
+    reference_split = ''
+    for o in options:
+        if o in content:
+            #print(f'found {o}')
+            reference_split = content.split(o)[-1]
 
-if len(reference_split) > 0:
-    page_number = re.search(r'[0-9]+\n', reference_split)
-    end = page_number.start() if page_number else -1
+    if len(reference_split) > 0:
+        page_number = re.search(r'[0-9]+\n', reference_split)
+        end = page_number.start() if page_number else -1
 
-reference_split = clean_string(reference_split)
-content = clean_string(content)
+    reference_split = clean_string(reference_split)
+    content = clean_string(content)
 
-count = 0
+    count = 0
 
-authors = '([A-Za-z,\n .\-]+)'
-title = '([A-Za-z–0-9:\- \n]+)'
-journal = '([A-Za-z–0-9:\- .,()&]+)(?=, [A-Za-z]{0,5} ?[0-9]{4}\.)'
-date = '(, [A-Za-z]{0,5} ?[0-9]{4}\.)'
-citation_regex = rf"({authors}\.\s{title}\.\s{journal}{date})"
+    authors = '([A-Za-z,\n .\-]+)'
+    title = '([A-Za-z–0-9:\- \n]+)'
+    journal = '([A-Za-z–0-9:\- .,()&]+)(?=, [A-Za-z]{0,5} ?[0-9]{4}\.)'
+    date = '(, [A-Za-z]{0,5} ?[0-9]{4}\.)'
+    citation_regex = rf"({authors}\.\s{title}\.\s{journal}{date})"
 
-print(reference_split[:1000])
+    #print(reference_split[:1000])
 
-citation = re.compile(citation_regex)
-for match in re.findall(citation, reference_split):
-    print(f'CITATION: {match[0]}')
-    print()
-    count += 1
+    out = { "Citations" : [] }
 
-print(f'{count} citations')
-print(repr(citation_regex))
+    citation = re.compile(citation_regex)
+    for match in re.findall(citation, reference_split):
+        #print(f'CITATION: {match[0]}')
+        #print()
+        out["Citations"].append(match[0])
+        count += 1
+
+    #print(f'{count} citations')
+    #print(repr(citation_regex))
+    
+    return out
+
+
+def get_papers():
+    try:
+        os.makedirs('./pdfs')
+        html = requests.get('https://arxiv.org/list/cs/new').text
+        soup = BeautifulSoup(html,'html.parser')
+        pdfs = soup.find_all('a',title="Download PDF")
+        links = []
+        for f in pdfs: links.append(f.get('href'))
+
+        for i in range(10):
+            if i < len(links):
+                title = links[i].split("/")[2]
+                content = requests.get("http://arxiv.org" + links[i] + ".pdf").content
+                with open(f'./pdfs/{title}.pdf', "wb") as f:
+                    f.write(content)
+            else:
+                break
+
+        for pdf in os.listdir('./pdfs'):
+            file_path = os.path.join('./pdfs',pdf)
+            print(file_path)
+            print(get_citations(file_path))
+
+    finally:
+        print("done")
+        shutil.rmtree('./pdfs')
+
+
+get_papers()
